afd9bd864cfc10d1e616d7a8d1363bcc08ce0ec6
Taylor <97355963+TaylorFlynn@users.noreply.github.com>
2022-02-13 15:32:51 -0800

    Reorder Imports

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
@@ -6,7 +7,6 @@ const authRoutes = require("./routes/auth.js");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
